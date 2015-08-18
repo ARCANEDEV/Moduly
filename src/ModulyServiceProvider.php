f5ccb963f6c5e69b965f2287e077589f0a8a77a7
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-08-18 17:28:12 +0100

    Refactoring the main service provider

--- src/ModulyServiceProvider.php
+++ src/ModulyServiceProvider.php
@@ -1,78 +1,76 @@
 <?php namespace Arcanedev\Moduly;
 
-use Arcanedev\Moduly\Contracts\ModuleRepositoryInterface;
-use Arcanedev\Moduly\Providers\CommandsServiceProvider;
-use Arcanedev\Moduly\Providers\MigrationServiceProvider;
-use Arcanedev\Moduly\Providers\RepositoryServiceProvider;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\ServiceProvider;
+use Arcanedev\Support\Laravel\PackageServiceProvider;
 
 /**
  * Class ModulyServiceProvider
- * @package Arcnaedev\Moduly
+ * @package Arcanedev\Moduly
  */
-class ModulyServiceProvider extends ServiceProvider
+class ModulyServiceProvider extends PackageServiceProvider
 {
-    /* ------------------------------------------------------------------------------------------------
-     |  Contants
-     | ------------------------------------------------------------------------------------------------
-     */
-    const MODULY_KEY = 'arcanedev.moduly';
-
     /* ------------------------------------------------------------------------------------------------
      |  Properties
      | ------------------------------------------------------------------------------------------------
      */
-    /**
-     * @var bool $defer Indicates if loading of the provider is deferred.
-     */
-    protected $defer = false;
+    protected $package = 'moduly';
 
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
      | ------------------------------------------------------------------------------------------------
      */
     /**
-     * Boot the service provider.
-     *
-     * @return void
+     * Register the service provider.
      */
-    public function boot()
+    public function register()
     {
-        $this->publishes([
-            __DIR__ . '/../config/moduly.php' => config_path('moduly.php'),
-        ]);
+        $this->registerConfigs();
+        $this->registerServices();
+        $this->registerFacades();
+        $this->registerProviders();
 
         moduly()->register();
     }
 
     /**
-     * Register the service provider.
+     * Get the services provided by the provider.
      *
-     * @return void
+     * @return array
      */
-    public function register()
+    public function provides()
     {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/moduly.php', 'moduly'
-        );
+        return ['arcanedev.moduly'];
+    }
 
-        $this->app->register(RepositoryServiceProvider::class);
-        $this->app->register(MigrationServiceProvider::class);
-        $this->app->register(CommandsServiceProvider::class);
-
-        $this->app->singleton(self::MODULY_KEY, function (Application $app) {
-            return new Moduly($app, $app->make(ModuleRepositoryInterface::class));
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register services
+     */
+    private function registerServices()
+    {
+        $this->app->bind(Moduly::KEY_NAME, function($app) {
+            return new Moduly($app, $app['config']);
         });
     }
 
     /**
-     * Get the services provided by the provider.
-     *
-     * @return string
+     * Register facades
      */
-    public function provides()
+    private function registerFacades()
     {
-        return [self::MODULY_KEY];
+        $this->addAliases([
+            'Moduly' => Facades\Moduly::class
+        ]);
+    }
+
+    /**
+     * Register providers
+     */
+    private function registerProviders()
+    {
+        $this->app->register(Providers\MigrationServiceProvider::class);
+        $this->app->register(Providers\CommandsServiceProvider::class);
     }
 }
